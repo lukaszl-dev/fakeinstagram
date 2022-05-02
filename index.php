b1fbd9e74067379ba24474b3e33a177daabf46ba
Łukasz <104734011+lukaszl-dev@users.noreply.github.com>
2022-05-02 09:23:13 +0200

    First step of project - UI/UX [number one].
    Now it isn't responsive in 100%. This is only visual not functional.
    TO-DO: 
    Make it responsive
    Make it functional
    Maybe Make better UI/UX

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type='text/css' href="style.css">
+    <title>FakeInstagram</title>
+
+    <!--FontAwsome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
+</head>
+<body>
+    <nav>
+        <div class="content logo">Instagram</div>
+        <div class="content">
+            <input class="look" name='search' type="text" placeholder='Szukaj'>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="content">
+            <div class="icons">
+                <i class="fa-solid fa-house"></i>
+                <i class="fa-solid fa-paper-plane"></i>
+                <i class="fa-solid fa-plus"></i>
+                <i class="fa-solid fa-compass"></i>
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="relations">
+            <div class="relation"><img src="img/html-1.png" alt="Relacja 1"></div>
+            <div class="relation"><img src="img/html-1.png" alt="Relacja 2"></div>
+            <div class="relation"><img src="img/html-1.png" alt="Relacja 3"></div>
+            <div class="relation"><img src="img/html-1.png" alt="Relacja 4"></div>
+            <div class="relation"><img src="img/html-1.png" alt="Relacja 5"></div>
+        </div>
+    </header>
+    <section>
+        <div class="box">
+            <div class="people">
+                <div class="people-img"><img src="img/html-1.png" alt="Relacja 1"></div> 
+                <div class="people-title">Łukasz Ligocki</div>
+                <div class="people-interact">...</div>
+            </div>
+            <div class="post-img">
+                <img src="img/html-1.png" alt="Post">
+            </div>
+            <div class="post-interact">
+                <div class="interact-icons">
+                    <i class="fa-solid fa-heart"></i>
+                    <i class="fa-regular fa-comment"></i>
+                    <i class="fa-solid fa-paper-plane"></i>
+                </div>
+                <div class="likes">
+                    Lubi to 10 użytkowników!
+                </div>
+                <div class="titlep">
+                    <strong>Łukasz Ligocki</strong> No to w drogę...
+                </div>
+                <div class="time">
+                    6 Godzin temu
+                </div>
+            </div>
+            <div class="coment">
+                <div class="comment-emoji">
+                    <i class="fa-regular fa-face-grin"></i>
+                </div>
+                <div class="comment-text">
+                    Dodaj komentarz...
+                </div>
+                <div class="comment-send">
+                    Opublikuj
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="script.js"></script>
+</body>
+</html> 
